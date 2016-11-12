0f6636e0b0992374a5d0a863e365c441b099d677
s3512667 <s3512667@student.rmit.edu.au>
2016-11-12 16:08:23 +1100

    Users page working without photo

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -2,6 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UsersRequest;
+
+use App\User;
+
+use App\Role;
+
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -15,7 +21,9 @@ class AdminUsersController extends Controller
      */
     public function index()
     {
-        return view('admin.users.index');
+        $users = User::all();
+
+        return view('admin.users.index', compact('users'));
     }
 
     /**
@@ -26,7 +34,10 @@ class AdminUsersController extends Controller
     public function create()
     {
         //
-        return view('admin.uses.create');
+
+        $roles = Role::lists('name','id')->all();
+
+        return view('admin.users.create',compact('roles'));
     }
 
     /**
@@ -35,9 +46,14 @@ class AdminUsersController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(UsersRequest $request)
     {
-        //
+        User::create($request->all());
+
+        return redirect('/admin/users');
+
+
+
     }
 
     /**
@@ -49,7 +65,7 @@ class AdminUsersController extends Controller
     public function show($id)
     {
         //
-        return view('admin.uses.show');
+        return view('admin.users.show');
     }
 
     /**
@@ -61,7 +77,7 @@ class AdminUsersController extends Controller
     public function edit($id)
     {
         //
-        return view('admin.uses.edit');
+        return view('admin.users.edit');
     }
 
     /**
