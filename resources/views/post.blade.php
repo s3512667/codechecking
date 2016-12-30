133bbaebff4231ac51ca7f7296fc29f890468e07
s3512667 <s3512667@student.rmit.edu.au>
2016-12-30 13:16:42 +1100

    The comments are displayed

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -51,6 +51,7 @@
             {!! Form::open(['method' => 'POST', 'action' => 'PostCommentsController@store', 'files'=> true]) !!}
 
                 <input type="hidden" name="post_id" value="{{$post->id}}" >
+
                 <div class="form-group">
                     {!! Form::label('body', 'Body:') !!}
                     {!! Form::textarea('body', null, ['class' => 'form-control', 'rows'=>3]) !!}
@@ -74,7 +75,11 @@
     <!-- Posted Comments -->
     @if(count($comments) > 0)
 
+        {{--<h1>The Comments are.....</h1>--}}
+
         @foreach($comments as $comment)
+
+
             <!-- Comment -->
             <div class="media">
                 <a class="pull-left" href="#">
@@ -85,31 +90,42 @@
                         <small>{{$comment->created_at->diffForHumans()}}</small>
                     </h4>
                     <p>{{$comment->body}}</p>
-                    {{--Nested Comment--}}
-                    <div class="media">
-                        <a class="pull-left" href="#">
-                            <img class="media-object" src="http://placehold.it/64x64" alt="">
-                        </a>
-                        <div class="media-body">
-                            <h4 class="media-heading">Nested Start Bootstrap
-                                <small>August 25, 2014 at 9:30 PM</small>
-                            </h4>
-                            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-                        </div>
-                        {!! Form::open(['method' => 'POST', 'action' => 'CommentRepliesController@createReply']) !!}
 
-                            <div class="form-group">
-                                {!! Form::label('body', 'Body:') !!}
-                                {!! Form::textarea('body', null, ['class' => 'form-control', 'rows'=>1]) !!}
+                    @if(count($comment->replies) > 0)
+
+                        @foreach($comment->replies as $reply)
+
+                            Nested Comment
+                            <div class="media">
+                                <a class="pull-left" href="#">
+                                    <img class="media-object" src="http://placehold.it/64x64" alt="">
+                                </a>
+                                <div class="media-body">
+                                    <h4 class="media-heading">Nested Start Bootstrap
+                                        <small>August 25, 2014 at 9:30 PM</small>
+                                    </h4>
+                                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                                </div>
+
+                                {!! Form::open(['method' => 'POST', 'action' => 'CommentRepliesController@createReply']) !!}
+
+                                    <div class="form-group">
+
+                                        <input type="hidden" name="comment_id" value="{{$comment->id}}" >
+
+                                        {!! Form::label('body', 'Body:') !!}
+                                        {!! Form::textarea('body', null, ['class' => 'form-control', 'rows'=>1]) !!}
+                                    </div>
+
+                                    <div class="form-group">
+                                        {!! Form::submit('Submit', ['class' => 'btn btn-primary']) !!}
+                                    </div>
+                                {!! Form::close()  !!}
                             </div>
+                            End Nested Comment
 
-                            <div class="form-group">
-                                {!! Form::submit('Submit', ['class' => 'btn btn-primary']) !!}
-                            </div>
-                        {!! Form::close()  !!}
-
-                    </div>
-                    {{--End Nested Comment--}}
+                        @endforeach
+                    @endif
                 </div>
             </div>
 
